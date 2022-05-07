7a011c88060b2d11063147bed84e72e26a37b4cc
Juanma Romero <xjuanma.romerox@gmail.com>
2022-05-07 17:12:21 -0400

    primero ico

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x974db97F8D5c2340CD37D81cF53Ab5deBDB33bc1";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
